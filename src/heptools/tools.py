d1a7b48f4c8f1a009b07669a3ea1cbb243ac735c
Ethan Lewis Simpson <ethansimpson285@gmail.com>
2022-04-14 22:41:17 +0200

    bringing tools together

--- src/heptools/tools.py
+++ src/heptools/tools.py
@@ -1,13 +1,27 @@
+import numpy as np
+
+def get_bin_values(hist):
+    return np.asarray([hist.GetBinContent(binn+1) for binn in range(0,hist.GetNbinsX())])
+
+def get_bin_errors(hist):
+    return np.asarray([hist.GetBinError(binn+1) for binn in range(0,hist.GetNbinsX())])
+
+def get_bin_edges(hist):
+    return np.asarray([hist.GetXaxis().GetBinUpEdge(binn) for binn in range(0,hist.GetXaxis().GetNbins()+1)])
+
+def get_bin_centres(hist):
+    return np.asarray([hist.GetXaxis().GetBinCenter(binn+1) for binn in range(0,hist.GetNbinsX())])
+
+
 def compute_ratio(ROOT_hist_numer,ROOT_hist_denom):
 
     d_hist = ROOT_hist_numer.Clone()
     d_hist.Divide(ROOT_hist_numer,ROOT_hist_denom)
-
     return d_hist
 
 
 def print_key_names(file):
-    [print(k.GetName()) for k in file.GetListOfKeys()]
+    return [k.GetName() for k in file.GetListOfKeys()]
 
 def print_branch_names(ttree):
-    [print(k.GetName()) for k in ttree.GetListOfBranches()]    
+    return [k.GetName() for k in ttree.GetListOfBranches()]        
