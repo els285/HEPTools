71b7ae26a9ba3093034c09443e1ee8b3218a394b
Ethan Lewis Simpson <ethansimpson285@gmail.com>
2022-04-14 22:43:28 +0200

    more tools updates

--- src/heptools/tools.py
+++ src/heptools/tools.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-def get_bin_values(hist):
+def bin_values(hist):
     return np.asarray([hist.GetBinContent(binn+1) for binn in range(0,hist.GetNbinsX())])
 
-def get_bin_errors(hist):
+def bin_errors(hist):
     return np.asarray([hist.GetBinError(binn+1) for binn in range(0,hist.GetNbinsX())])
 
-def get_bin_edges(hist):
+def bin_edges(hist):
     return np.asarray([hist.GetXaxis().GetBinUpEdge(binn) for binn in range(0,hist.GetXaxis().GetNbins()+1)])
 
-def get_bin_centres(hist):
+def bin_centres(hist):
     return np.asarray([hist.GetXaxis().GetBinCenter(binn+1) for binn in range(0,hist.GetNbinsX())])
 
 
@@ -20,8 +20,8 @@ def compute_ratio(ROOT_hist_numer,ROOT_hist_denom):
     return d_hist
 
 
-def print_key_names(file):
+def key_names(file):
     return [k.GetName() for k in file.GetListOfKeys()]
 
-def print_branch_names(ttree):
+def branch_names(ttree):
     return [k.GetName() for k in ttree.GetListOfBranches()]        
