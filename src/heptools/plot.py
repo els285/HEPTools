1cc2a5657d9e713c49092a2bc99d9c71119696ef
Ethan Simpson <ethansimpson@android-91d188c293e7433a.home>
2022-10-25 00:36:30 +0200

    1d hist plot functionality for python3.10

--- src/heptools/plot.py
+++ src/heptools/plot.py
@@ -3,16 +3,42 @@ import matplotlib.pyplot as plt
 
 def atlas_1d(list_of_hists,**kwargs):
 
-    # Main
+    """
+    Script for generating (non-ratio) plot of ROOT TH1 histograms
+    """
+
+    # Style
     hep.style.use(hep.style.ATLAS) 
-    hep.histplot(list_of_hists)
+
+    if any([x in kwargs and x for x in ["norm","Norm","normalise","Normalise"]]):
+        hists_to_plot = []
+        for h in list_of_hists:
+            hc = h.Clone(h.GetName())
+            hc.Scale(1/hc.Integral())
+            hists_to_plot.append(hc)
+    else:
+        hists_to_plot = list_of_hists
+
+    # Plot
+    hep.histplot(hists_to_plot)
 
     # Accompanying text
-    if "text" in kwargs:
+    if "text" in kwargs and not "label" in kwargs:
         hep.atlas.text(kwargs["text"])
-    else:
+    elif not "text" in kwargs and not "label" in kwargs:
         hep.atlas.text("Preliminary")
 
+    # Label
+    if "label" in kwargs:
+        if isinstance(kwargs["label"],dict):
+            data_label = kwargs["label"]
+        elif isinstance(kwargs["label"],bool) and kwargs["label"]:
+            data_label = {"text"  : "",
+                            "data": False,
+                            "lumi": 139,
+                            "year": 2016}
+
+        hep.atlas.label(data_label["text"], data=data_label["data"], lumi=data_label["lumi"], year=data_label["year"])
 
     # Legend
     if "legend" in kwargs:
