ff3bd9c17dbd2944fa8e49fbfb70b2cc447462b9
Ethan <ethansimpson285@gmail.com>
2023-10-19 19:33:09 +0100

    correct definition of spin obsevables

--- src/heptools/spin_observables.py
+++ src/heptools/spin_observables.py
@@ -1,5 +1,4 @@
-import vector
-
+import vector 
 
 def boost_to_parent_tops(top:'MomentumObject4D',anti_top:'MomentumObject4D',
              leptonP:'MomentumObject4D',leptonM:'MomentumObject4D'):
@@ -43,22 +42,29 @@ def cos_k(Kdirection,leptonP_direction,leptonM_direction):
 
 def cos_n(Kdirection,leptonP_direction,leptonM_direction):
 
-    z = vector.obj(x=0,y=0,z=1)
-    Ndirection = z.cross(Kdirection)/(1-(z.dot(Kdirection))**2)*0.5
+    z     = vector.obj(x=0,y=0,z=1)
+    cos_T = z.dot(Kdirection)
+    sin_T = (1 - cos_T**2)**0.5
+    Ndirection = z.cross(Kdirection)/sin_T
 
-    cos_plus  = Ndirection.dot(leptonP_direction)
-    cos_minus = -Ndirection.dot(leptonM_direction) 
+    A = 1 if cos_T > 0 else -1
+
+    cos_plus  = A*Ndirection.dot(leptonP_direction)
+    cos_minus = -A*Ndirection.dot(leptonM_direction) 
     return cos_plus, cos_minus
 
 
 def cos_r(Kdirection,leptonP_direction,leptonM_direction):
 
-    z = vector.obj(x=0,y=0,z=1)
-    y = z.dot(Kdirection)
-    Rdirection =  y / (1-y**2)*0.5 * (z - Kdirection)
+    z     = vector.obj(x=0,y=0,z=1)
+    cos_T = z.dot(Kdirection)
+    sin_T = (1 - cos_T**2)**0.5
+    Rdirection =  1/sin_T * (z - cos_T*Kdirection)
 
-    cos_plus  = Rdirection.dot(leptonP_direction)
-    cos_minus = -Rdirection.dot(leptonM_direction) 
+    A = 1 if cos_T > 0 else -1
+
+    cos_plus  = A*Rdirection.dot(leptonP_direction)
+    cos_minus = -A*Rdirection.dot(leptonM_direction) 
     return cos_plus, cos_minus
 
 
@@ -68,6 +74,11 @@ def cos_phi(leptonP_direction,leptonM_direction):
 
 
 def helicity_basis_angular_observables(top,antitop,lepP,lepM):
+
+    """
+    Builds the helicity-basis angular observables plus cos_phi
+    """
+
     Kdirection, lepP_direction, lepM_direction = boost_to_parent_tops(top,antitop,lepP,lepM)
     obs = {}
     obs["cos_k_p"], obs["cos_k_m"] = cos_k(Kdirection,lepP_direction,lepM_direction)
@@ -75,3 +86,6 @@ def helicity_basis_angular_observables(top,antitop,lepP,lepM):
     obs["cos_r_p"], obs["cos_r_m"] = cos_r(Kdirection,lepP_direction,lepM_direction)
     obs["cos_phi"]                 = cos_phi(lepP_direction,lepM_direction)
     return obs
+
+def lab_frame_cos_phi(lepP,lepM):
+    return lepP.to_beta3().unit().dot(lepM.to_beta3().unit())
